7eee48e3b2108028890f0149eaef79d3227c9b8d
JonsMota <jonsmota@gmail.com>
2023-03-02 16:33:37 -0300

    Criado o Title, Button e o Input

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,20 @@
+import Title from '../src/components/title/Title'
+import Button from '../src/components/button/Button'
+import Input from '../src/components/input/Input'
+
 export default function Home() {
-  return <div>Perfil do usuário</div>
+  return (
+    <>
+      <Title>Teste</Title>
+      <label>Voltar a página...</label>
+      <form>
+        <Input type="text" placeholder="Digite seu e-mail" />
+        <Input type="password" placeholder="Digite sua senha" />
+        <Button>Botão</Button>
+      </form>
+      <Button>Botão</Button>
+      <label>LABEL TESTE </label>
+      <button>button</button>
+    </>
+  )
 }
---
+++ src/components/button/Button.jsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 400px;
+
+    label {
+      font-weight: bold;
+      color: red;
+    }
+
+    button {
+      background-color: ${(props) => props.theme.primary};
+      cursor: pointer;
+      font-weight: bold;
+      border: 0;
+      padding: 15px;
+      border-radius: 5px;
+    }
+`
+
+export default function Button(props) {
+  return (
+    <ButtonContainer>
+      <label>Button Label</label>
+      <button {...props}></button>
+    </ButtonContainer>
+  )  
+}
