3bf388e13590a356fa8f7bc8ddd58a125f85ca58
JonsMota <jonsmota@gmail.com>
2023-03-02 21:56:09 -0300

    Feito a lógica para pegar as informações do input

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
+import { useState } from 'react'
+
 import Title from '../src/components/title/Title'
 import Button from '../src/components/button/Button'
 import Input from '../src/components/input/Input'
 
 export default function Home() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   const handleClick = () => {
     console.log('Handle Click')
   }
@@ -10,6 +15,15 @@ export default function Home() {
   const handleSubmitForm = (event) => {
     event.preventDefault()
     console.log('Handle Submit Form')
+    console.log({ email, password })
+  }
+
+  const handleChangeInput = (event) => {
+    setEmail(event.target.value)
+  }
+
+  const handleChangeInputPassword = (event) => {
+    setPassword(event.target.value)
   }
 
   return (
@@ -17,8 +31,9 @@ export default function Home() {
       <Title onClick={handleClick}>Teste</Title>
       <label onClick={handleClick}>Voltar a página...</label>
       <form onSubmit={handleSubmitForm}>
-        <Input type="text" placeholder="Digite seu e-mail" />
-        <Input type="password" placeholder="Digite sua senha" />
+        <Input type="text" placeholder="Digite seu e-mail" onChange={handleChangeInput} />
+        <Input type="password" placeholder="Digite sua senha" onChange={handleChangeInputPassword} />
+        <p>O e-mail digitado é: {email} </p>
         <Button type="submit">Botão DENTRO do form</Button>
       </form>
       <Button onClick={handleClick}>Botão FORA do form</Button>
