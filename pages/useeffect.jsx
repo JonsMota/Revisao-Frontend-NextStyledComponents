ea87edafee9af978e84881f5a92c5241f9c4fcab
JonsMota <jonsmota@gmail.com>
2023-03-03 11:32:42 -0300

    Criado formulário e o input para adicionar nomes na lista

--- pages/useeffect.jsx
+++ pages/useeffect.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react'
 
+import Input from '../src/components/input/Input'
+
 export default function useEffectPage() {
+  const [name, setName] = useState('')
   const [userList, setUserList] = useState([])  
 
   useEffect(() => {
@@ -21,11 +24,31 @@ export default function useEffectPage() {
     console.log('Lista modificada')
   }, ['Lista modficada'])
 
+  const handleForm = (event) => {
+    event.preventDefault()
+    setUserList([
+      ...userList,
+      {
+        name
+      }
+    ])
+
+    setName('')
+  }
+
   return (
     <div>
       {userList.map((user, key) => (
         <div key={`user-${key}`}>{user.name}</div>
       ))}
+      <form onSubmit={handleForm}>
+        <Input
+          type="text"
+          placeholder="Nome"
+          value={name}
+          onChange={() => setName(event.target.value)}
+        />
+      </form>
     </div>
   )
 }
