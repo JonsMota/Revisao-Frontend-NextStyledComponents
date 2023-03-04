f4a0d599a37f3370fe862ad10a34228ae390bba1
JonsMota <jonsmota@gmail.com>
2023-03-04 18:11:17 -0300

    Criado a função setTimeout

--- pages/debounce.jsx
+++ pages/debounce.jsx
@@ -1,17 +1,21 @@
 import { useRef, useState, useEffect } from 'react'
 
 export default function DebouncePage() {
+  const debounce = useRef(null)    
   const [text, setText] = useState('')  
 
   const searchData = () => {
     if (!text) return
-    
+
     console.log('... searching data ...')
     console.log(text)
   }
 
   useEffect(() => {
-    searchData()
+    if (debounce.current) clearInterval(debounce.current)
+    debounce.current = setTimeout(searchData, 2000)
+
+    return () => clearInterval(debounce.current)
   }, [text]) 
   
   return (
